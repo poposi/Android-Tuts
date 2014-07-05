9363ccb40a25e488ba088728d429108a4a9d93eb
Tran Phuc Thien <tranphucthien@gmail.com>
2014-07-05 14:19:43 +0700

    Add ListviewDemo

---
+++ SimpleToastMessage/gen/poposi/newtypefoundation/simpletoastmessage/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package poposi.newtypefoundation.simpletoastmessage;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
